86603ca67627d10cd9ab5ce83b29d4cddac285d4
Christine QE <christine.q.ernst@gmail.com>
2017-10-20 02:31:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,15 +26,11 @@ function prependKitten(name) {
 function removeLastKitten() {
   return kittens.slice(0, kittens.length-1);
 }
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
 /*
-
-  describe('removeLastKitten()', function() {
-    it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-      expect(removeLastKitten()).toEqual(["Milo", "Otis"])
-
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-    })
-  })
 
   describe('removeFirstKitten()', function() {
     it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
